e05e857004b084b251ef223670a2b15c5ce4e7e2
Peanut <110200246+JsPeanut@users.noreply.github.com>
2023-06-04 19:44:46 -0300

    Update Program.cs

--- TestingArea/TestingArea/Program.cs
+++ TestingArea/TestingArea/Program.cs
@@ -1,51 +1,150 @@
-﻿using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-Console.Clear();
-Console.OutputEncoding = Encoding.UTF8;
-Console.CursorVisible = false;
-Console.ForegroundColor = ConsoleColor.Cyan;
-Console.WriteLine("Welcome to the world of C#!");
-Console.ResetColor();
-Console.WriteLine("\nUse ⬆️  and ⬇️  to navigate and press \u001b[32mEnter/Return\u001b[0m to select:");
-(int left, int top) = Console.GetCursorPosition();
-var selectedOption = 0;
-var decorator = "✅ \u001b[32m";
-ConsoleKeyInfo key;
-bool isSelected = false;
-
-var options = new[]
+namespace TestingArea
 {
-    "Bulbasaur",
-    "Charmander",
-    "Squirtle"
-};
-
-while (!isSelected)
-{
-    Console.SetCursorPosition(left, top);
-
-    for (int i = 0; i < options.Length; i++)
+    class Program
     {
-        Console.WriteLine($"{(selectedOption == i ? decorator : "   ")}{options[i]}\u001b[0m");
-    }
+        public static int ShowMenu(string arrow1, string arrow2, int options, string welcomemsg = "", string arrowstousemsg = "", string opt1 = "", string opt2 = "", string opt3 = "", string opt4 = "", string opt5 = "", string opt6 = "")
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            if (welcomemsg == "")
+            {
 
-    key = Console.ReadKey(false);
+            }
+            else
+            {
+                Console.WriteLine($"{welcomemsg}");
+            }
+            Console.ResetColor();
+            if (arrowstousemsg == "")
+            {
 
-    switch (key.Key)
-    {
-        case ConsoleKey.UpArrow:
-            selectedOption = selectedOption == 0 ? options.Length - 1 : selectedOption - 1;
-            break;
+            }
+            else
+            {
+                Console.WriteLine($"{arrowstousemsg}");
+            }
+            (int left, int top) = Console.GetCursorPosition();
+            var option = 1;
+            var decorator = $"✅ \u001b[32m";
+            ConsoleKeyInfo key;
+            bool isSelected = false;
 
-        case ConsoleKey.DownArrow:
-            selectedOption = selectedOption == options.Length - 1 ? 0 : selectedOption + 1;
-            break;
+            while (!isSelected)
+            {
+                Console.SetCursorPosition(left, top);
 
-        case ConsoleKey.Enter:
-            isSelected = true;
-            break;
+                if (arrow1 == "LeftArrow" && arrow2 == "RightArrow")
+                {
+                    if (!(opt1 == null) || !(opt1 == ""))
+                    {
+                        Console.Write($"{(option == 1 ? decorator : "  ")}{opt1}\u001b[0m");
+                    }
+                    if (!(opt2 == null) || !(opt2 == ""))
+                    {
+                        Console.Write($"{(option == 2 ? decorator : "  ")}{opt2}\u001b[0m");
+                    }
+                    if (!(opt3 == null) || !(opt3 == ""))
+                    {
+                        Console.Write($"{(option == 3 ? decorator : "  ")}{opt3}\u001b[0m");
+                    }
+                    if (!(opt4 == null) || !(opt4 == ""))
+                    {
+                        Console.Write($"{(option == 4 ? decorator : "  ")}{opt4}\u001b[0m");
+                    }
+                    if (!(opt5 == null) || !(opt5 == ""))
+                    {
+                        Console.Write($"{(option == 5 ? decorator : "  ")}{opt5}\u001b[0m");
+                    }
+                    if (!(opt6 == null) || !(opt6 == ""))
+                    {
+                        Console.Write($"{(option == 6 ? decorator : "  ")}{opt6}\u001b[0m");
+                    }
+                }
+                else if (arrow1 == "UpArrow" && arrow2 == "DownArrow")
+                {
+                    if (!(opt1 == null) || !(opt1 == ""))
+                    {
+                        Console.WriteLine($"{(option == 1 ? decorator : "  ")}{opt1}\u001b[0m");
+                    }
+                    if (!(opt2 == null) || !(opt2 == ""))
+                    {
+                        Console.WriteLine($"{(option == 2 ? decorator : "  ")}{opt2}\u001b[0m");
+                    }
+                    if (!(opt3 == null) || !(opt3 == ""))
+                    {
+                        Console.WriteLine($"{(option == 3 ? decorator : "  ")}{opt3}\u001b[0m");
+                    }
+                    if (!(opt4 == null) || !(opt4 == ""))
+                    {
+                        Console.WriteLine($"{(option == 4 ? decorator : "  ")}{opt4}\u001b[0m");
+                    }
+                    if (!(opt5 == null) || !(opt5 == ""))
+                    {
+                        Console.WriteLine($"{(option == 5 ? decorator : "  ")}{opt5}\u001b[0m");
+                    }
+                    if (!(opt6 == null) || !(opt6 == ""))
+                    {
+                        Console.WriteLine($"{(option == 6 ? decorator : "  ")}{opt6}\u001b[0m");
+                    }
+                }
+                key = Console.ReadKey(false);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (arrow1 == "UpArrow")
+                        {
+                            option = option == 1 ? options : option - 1;
+                        }
+                        else
+                        {
+                        }
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        if (arrow2 == "DownArrow")
+                        {
+                            option = option == options ? 1 : option + 1;
+                        }
+                        else
+                        {
+                        }
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        if (arrow1 == "LeftArrow")
+                        {
+                            option = option == 1 ? options : option - 1;
+                        }
+                        else
+                        {
+                        }
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        if (arrow2 == "RightArrow")
+                        {
+                            option = option == options ? 1 : option + 1;
+                        }
+                        else
+                        {
+                        }
+                        break;
+
+                    case ConsoleKey.Enter:
+                        isSelected = true;
+                        break;
+                }
+            }
+            Console.WriteLine($"\n{decorator}You selected Option {option}");
+            Console.ResetColor();
+            return option;
+        }
     }
 }
-
-Console.WriteLine($"\n{decorator}You selected {options[selectedOption]}");
-Console.ReadLine();
